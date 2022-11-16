66cd1580ee6bd6bdbcce2e4617856107f6ca7063
Matheus-S-Costa <matheusilva334@gmail.com>
2022-11-16 11:25:32 -0300

    add aula 12

---
+++ aula 12/index.js
@@ -0,0 +1,19 @@
+/*
+function soma(x,y) {
+    const resultado = x+y;
+    return resultado
+}
+
+const resultado = soma(2,2);
+console.log(resultado);
+*/
+
+/* const raiz = function(n) {
+    return n ** 0.5;
+};
+console.log(raiz(9))
+*/
+
+const raiz = n => n**0.5;
+
+console.log(raiz(16))
